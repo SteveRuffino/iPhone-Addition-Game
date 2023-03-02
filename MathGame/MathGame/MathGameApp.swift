eddd3498e2b657a324a148d9de71a293e982503e
Steve Ruffino <122953366+SteveRuffino@users.noreply.github.com>
2023-03-02 00:06:23 -0600

    Add files via upload

---
+++ MathGame/MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Steve Ruffino on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
